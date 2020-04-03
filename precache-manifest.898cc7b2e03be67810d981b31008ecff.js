c43ca432f01ad2268dbed1cfe8f356d523ba45fd
sharma-sandy <sandeep.sharma@tigeranalytics.com>
2020-04-03 14:52:01 +0530

    Updates

---
+++ precache-manifest.898cc7b2e03be67810d981b31008ecff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68162b3681ca9255f2def43b0a23b1cc",
+    "url": "/ecome/index.html"
+  },
+  {
+    "revision": "071735f7077f242a2e5a",
+    "url": "/ecome/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30c2dffde8a327ae09fa",
+    "url": "/ecome/static/js/2.75992f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecome/static/js/2.75992f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071735f7077f242a2e5a",
+    "url": "/ecome/static/js/main.af98981b.chunk.js"
+  },
+  {
+    "revision": "669bfd6c96269469e98e",
+    "url": "/ecome/static/js/runtime-main.2ea8e690.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecome/static/media/logo.5d5d9eef.svg"
+  }
+]);
